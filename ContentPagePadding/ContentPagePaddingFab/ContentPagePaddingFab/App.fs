cb49da615d29d23b4c19526ff2129d6f06f74aaa
Kev Hannen <kev.hannen@gmail.com>
2023-04-20 20:34:36 +0100

    add Fab project

---
+++ ContentPagePadding/ContentPagePaddingFab/ContentPagePaddingFab/App.fs
@@ -0,0 +1,35 @@
+namespace ContentPagePaddingFab
+
+open Xamarin.Forms
+open Fabulous.XamarinForms
+
+open type View
+
+module App =
+    type Model = { Count: int }
+
+    type Msg =
+        | Increment
+        | Decrement
+
+    let init () = { Count = 0 }
+
+    let update msg model =
+        match msg with
+        | Increment -> { model with Count = model.Count + 1 }
+        | Decrement -> { model with Count = model.Count - 1 }
+
+    let view model =
+        Application(
+            ContentPage(
+                "ContentPagePaddingFab",
+                Frame(
+                    Label("Hello from Fabulous v2!")
+                        .centerTextHorizontal()
+                        .centerTextVertical()
+                )
+            )
+                .padding(50.)
+        )
+
+    let program = Program.stateful init update view
